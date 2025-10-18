9045dde15be418919dcff261082f6830b6adbcb6
anna <annakeiotto@gmail.com>
2025-10-18 14:53:51 -0500

    initial front end commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import control_panel from "./assets/control_panel.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={control_panel} alt="control panel" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
